230d7343019f29eb67e1de0edf54551ee925b017
marimiha <itmvmktgs@gmail.com>
2023-10-12 19:02:42 +0300

    init commit

---
+++ Базовые типы языка Python и работа с ними/Простые типы данных/Особенности работы с простыми типами/main.py
@@ -0,0 +1,21 @@
+int_ = 7  # Целочисленный тип данных
+print(int_, type(int_))
+
+float_ = 123.321  # Числа с плавающей запятой (дробные числа)
+print(float_, type(float_))
+
+bool_true = True  # Логический тип данных истина
+bool_false = False   # Логический тип данных ложь
+print(bool_true, bool_false, type(bool_true))
+
+empty = None  # Отсутствие результата это тоже результат
+print(empty, type(empty))
+int = 5
+print(int, type(int_))
+float = 123.321
+print(float, type(float))
+bool_true = True
+bool_false = False
+print(bool_true, bool_false,type(bool_true))
+empty = None
+print(empty, type(empty))
