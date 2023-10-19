edfa4176d26a704d043921fdf33bc0d204a5417d
kab2-3-16 <itmvmktgs@gmail.com>
2023-10-19 20:43:11 +0300

    linked stepik

--- Базовые типы языка Python и работа с ними/Простые типы данных/Арифметические операторы/main.py
+++ Базовые типы языка Python и работа с ними/Простые типы данных/Арифметические операторы/main.py
@@ -1,8 +1,6 @@
 # переменные лучше, чем "магические числа"
 # TODO Задайте длину и ширину футбольного поля
-
 length = 90
-width = 50
-perimeter = 2*(length+width)
-
+weidth =50
+perimeter = (length + weidth)*(2)
 print("Периметр футбольного поля равен", perimeter)
--- Базовые типы языка Python и работа с ними/Простые типы данных/Особенности работы с простыми типами/main.py
+++ Базовые типы языка Python и работа с ними/Простые типы данных/Особенности работы с простыми типами/main.py
@@ -1,4 +1,4 @@
-int_ = 7  # Целочисленный тип данных
+int_ = 5  # Целочисленный тип данных
 print(int_, type(int_))
 
 float_ = 123.321  # Числа с плавающей запятой (дробные числа)
@@ -9,13 +9,4 @@ bool_false = False   # Логический тип данных ложь
 print(bool_true, bool_false, type(bool_true))
 
 empty = None  # Отсутствие результата это тоже результат
-print(empty, type(empty))
-int = 5
-print(int, type(int_))
-float = 123.321
-print(float, type(float))
-bool_true = True
-bool_false = False
-print(bool_true, bool_false,type(bool_true))
-empty = None
 print(empty, type(empty))
